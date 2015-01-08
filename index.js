66b5cede6e0be79ec5fc1f0c15a9329af2385596
Ivan Kozik <ivan@ludios.org>
2015-01-08 16:30:26 +0000

    EXIT_ON_CHANGES -> EXIT_ON_CHANGES_ENABLED in case we need to add more options in the future

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
-if(Number(process.env.EXIT_ON_CHANGES)) {
-	console.log("[exit-on-changes] EXIT_ON_CHANGES enabled");
+if(Number(process.env.EXIT_ON_CHANGES_ENABLED)) {
+	console.log("[exit-on-changes] EXIT_ON_CHANGES_ENABLED");
 	fs.watch('.', {}, function(ev, filename) {
 		if(/\.(jsx?|es6?)$/.test(filename)) {
 			console.log("[exit-on-changes] Exiting after:", ev, filename);
