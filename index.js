b9f36b59de917e0c9c0220b25356bc27e7aab2c6
Ivan Kozik <ivan@ludios.org>
2015-01-11 03:44:55 +0000

    Use chokidar instead of fs.watch because fs.watch doesn't watch recursively

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var fs = require('fs');
+// Use chokidar instead of fs.watch because fs.watch doesn't watch recursively
+var chokidar = require('chokidar');
 
 if(Number(process.env.EXIT_ON_CHANGES_ENABLED)) {
 	console.log("[exit-on-changes] EXIT_ON_CHANGES_ENABLED");
-	fs.watch('.', {}, function(ev, filename) {
+	var watcher = chokidar.watch('.', {ignoreInitial: true, ignorePermissionErrors: true});
+	watcher.on('all', function(ev, filename) {
 		if(/\.(jsx?|es6?)$/.test(filename)) {
 			console.log("[exit-on-changes] Exiting after:", ev, filename);
 			process.exit(0);
